15645d7cb3d0ec01ba321f9dfbe508f295578ced
kiah.jones <kiah.jones@docusign.com>
2017-04-25 14:52:44 -0700

    Adding menu component

---
+++ orca/src/app/menu/menu.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-menu',
+  templateUrl: './menu.component.html',
+  styleUrls: ['./menu.component.css', './menu.component-mobile.css']
+})
+export class MenuComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
