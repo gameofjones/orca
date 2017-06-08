b7690db49fbdb9a1218a921413babb3ee51a1be2
kiah.jones <kiah.jones@docusign.com>
2017-06-08 14:29:09 -0700

    Adding in card animation in step

--- orca/src/app/get-a-card/get-a-card.component.ts
+++ orca/src/app/get-a-card/get-a-card.component.ts
@@ -1,18 +1,19 @@
-import { Component, OnInit, OnChanges, trigger, transition, style, animate, state } from '@angular/core';
+import { Component, OnInit, OnChanges, trigger, transition, style, animate, state, keyframes } from '@angular/core';
 
 @Component({
     selector: 'app-get-a-card',
     templateUrl: './get-a-card.component.html',
     styleUrls: ['../app.component.css', './get-a-card.component.css'],
     animations: [
-      trigger('flyInOut', [
-          state('in', style({transform: 'translateX(0)'})),
+      trigger('flyIn', [
+          state('in', style({ transform: 'translateY(0)' })),
           transition('void => *', [
-              style({transform: 'translateX(-100%)'}),
-              animate(100)
-          ]),
-          transition('* => void', [
-              animate(100, style({transform: 'translateX(100%)'}))
+              style({transform: 'translateY(30%)'}),
+              animate(1000, keyframes([
+                  style({ opacity: 0, transform: 'translateY(30%)', offset: 0 }),
+                  style({ opacity: 1, transform: 'translateY(-15px)',  offset: 0.3 }),
+                  style({ opacity: 1, transform: 'translateY(0)',     offset: 1.0})
+              ]))
           ])
       ])
     ]
@@ -21,13 +22,25 @@ import { Component, OnInit, OnChanges, trigger, transition, style, animate, stat
 export class GetACardComponent implements OnInit {
 
     imageLoaded: boolean = false;
+    // keeps track of subsequent image loading
+    imageDelay: Array<boolean> = [false, false];
 
-    constructor() { }
+    constructor() {
+        this.imageDelay = [false, false];
+     }
 
     ngOnInit() {
     }
 
-    onLoad(id) {
+    onImageLoad() {
+      // triggers first animation
         this.imageLoaded = true;
+    }
+
+    animateNext(imageNumber) {
+        // store global property here accurately reference it
+        var imageDelay = this.imageDelay;
+        // trigger animation of the next card
+        setTimeout(function () { imageDelay[imageNumber] = true; }, 50)
     }
 }
